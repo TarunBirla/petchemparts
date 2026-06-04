aaafb4609707e7abb4233eefb5170d1c5893f33f
Tarun Birla <tarunbirla2018@gmail.com>
2026-06-04 20:04:40 +0530

    section

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -18,4 +18,17 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css
     <link href="{{asset('backend/css/sb-admin-2.min.css')}}" rel="stylesheet">
     @stack('styles')
   
+     <!-- Bootstrap core JavaScript-->
+  <script src="{{asset('backend/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  
+
+  <!-- Core plugin JavaScript-->
+  <script src="{{asset('backend/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="{{asset('backend/js/sb-admin-2.min.js')}}"></script>
+
+  <!-- Page level plugins -->
+  <script src="{{asset('backend/vendor/chart.js/Chart.min.js')}}"></script>
 </head>

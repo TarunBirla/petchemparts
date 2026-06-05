1ccf1cf3964110da7d1fb750c5c1dd333a944721
Tarun Birla <tarunbirla2018@gmail.com>
2026-06-05 15:37:04 +0530

    section

--- resources/views/user/layouts/master.blade.php
+++ resources/views/user/layouts/master.blade.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+  <!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link href="{{asset('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+ 
+<!-- SB Admin base (kept for JS compatibility) -->
+<link href="{{asset('backend/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
 @include('user.layouts.head')
 
@@ -18,6 +24,12 @@
       <!-- End of Main Content -->
       @include('user.layouts.footer')
 
+      <!-- JS (head) -->
+<script src="{{asset('backend/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('backend/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+<script src="{{asset('backend/js/sb-admin-2.min.js')}}"></script>
+<script src="{{asset('backend/vendor/chart.js/Chart.min.js')}}"></script>
 </body>
 
 </html>
